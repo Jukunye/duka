fbf662b709c68cddb588889f2f856619e8e1dfa9
Jukunye <jukunyes@gmail.com>
2024-06-05 20:51:56 +0300

    Implementing the authentication guard in user service

--- services/user-service/src/auth/auth.controller.ts
+++ services/user-service/src/auth/auth.controller.ts
@@ -1,14 +1,31 @@
-import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Post,
+  Request,
+  UseGuards
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { AuthGuard } from './auth.guard';
+import { Public } from './constants';
 
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @HttpCode(HttpStatus.OK)
+  @Public()
   @Post('login')
   SignIn(@Body() signInDto: Record<string, any>) {
     // TODO: see validation chapter -> (Record)
     return this.authService.signIn(signInDto.username, signInDto.password);
+  }
+
+  @Get('profile')
+  getProfile(@Request() req) {
+    return req.user;
   }
 }
