3802b8ca4bf896126958dc47579032c12d43b816
Jukunye <jukunyes@gmail.com>
2024-06-05 18:45:51 +0300

    Authentication: user service with JWT

--- services/user-service/src/auth/auth.controller.ts
+++ services/user-service/src/auth/auth.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  SignIn(@Body() signInDto: Record<string, any>) {
+    // TODO: see validation chapter -> (Record)
+    return this.authService.signIn(signInDto.username, signInDto.password);
+  }
+}
