ba232acc54445928c9ef587617e521d5d71ad751
Jukunye <jukunyes@gmail.com>
2024-06-06 21:51:11 +0300

    feat: :zap: Add user registration, profile update, and delete functionality
    Add new endpoints to the AuthController for user registration, profile update, and delete. Update the SignIn endpoint to use a DTO for validation.

--- services/user-service/src/auth/auth.controller.ts
+++ services/user-service/src/auth/auth.controller.ts
@@ -1,16 +1,19 @@
 import {
   Body,
   Controller,
+  Delete,
   Get,
   HttpCode,
   HttpStatus,
+  Patch,
   Post,
   Request,
   UseGuards
 } from '@nestjs/common';
 import { AuthService } from './auth.service';
-import { AuthGuard } from './auth.guard';
 import { Public } from './constants';
+import { signInDto } from './dto/signInDto.dto';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
 
 @Controller('auth')
 export class AuthController {
@@ -18,14 +21,35 @@ export class AuthController {
 
   @HttpCode(HttpStatus.OK)
   @Public()
+  @Post('register')
+  signUp(@Body() createUsrDto: CreateUserDto) {
+    return this.authService.signUp(createUsrDto);
+  }
+
+  @HttpCode(HttpStatus.OK)
+  @Public()
   @Post('login')
-  SignIn(@Body() signInDto: Record<string, any>) {
-    // TODO: see validation chapter -> (Record)
-    return this.authService.signIn(signInDto.username, signInDto.password);
+  SignIn(@Body() signInDto: signInDto) {
+    return this.authService.signIn(signInDto);
+  }
+
+  @Get('all-users')
+  getUsers() {
+    return this.authService.all();
   }
 
   @Get('profile')
   getProfile(@Request() req) {
-    return req.user;
+    return this.authService.findOne(req.user.email);
+  }
+
+  @Patch('profile')
+  updateProfile(@Request() req, @Body() updateUserDto) {
+    return this.authService.update(req.user.email, updateUserDto);
+  }
+
+  @Delete('profile')
+  deleteProfile(@Request() req) {
+    return this.authService.delete(req.user.email);
   }
 }
