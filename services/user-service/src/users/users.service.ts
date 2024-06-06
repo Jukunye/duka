2651de021667bd35f8ad827e8266cd92c8fe694a
Jukunye <jukunyes@gmail.com>
2024-06-06 21:46:10 +0300

    feat: :sparkles: Add Mongoose-based CRUD operations to UsersService
    Add Mongoose-based CRUD operations to the UsersService, replacing the previous in-memory array implementation.  It also adds type annotations and imports for the User and UpdateUserDto classes.

--- services/user-service/src/users/users.service.ts
+++ services/user-service/src/users/users.service.ts
@@ -1,24 +1,49 @@
-import { Injectable } from '@nestjs/common';
-
-// This should be a real class/interface representing a user entity
-export type User = any;
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { User, UserDocument } from './users.schema';
+import { Model } from 'mongoose';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
 
 @Injectable()
 export class UsersService {
-  private readonly users = [
-    {
-      userId: 1,
-      username: 'john',
-      password: 'changeme'
-    },
-    {
-      userId: 2,
-      username: 'maria',
-      password: 'guess'
-    }
-  ];
+  constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
 
-  async findOne(username: string): Promise<User | undefined> {
-    return this.users.find((user) => user.username === username);
+  // CREATE
+  async create(CreateUserDto: CreateUserDto): Promise<User> {
+    const createdUser = new this.userModel(CreateUserDto);
+    return createdUser.save();
+  }
+
+  // READ
+  async findAll(): Promise<User[]> {
+    return this.userModel.find().exec();
+  }
+
+  async findOne(email: string): Promise<UserDocument> {
+    const user = await this.userModel.findOne({ email }).exec();
+    if (!user) {
+      throw new NotFoundException(`User with email ${email} not found`);
+    }
+    return user;
+  }
+
+  // UPDATE
+  async update(email: string, updateUserDto: UpdateUserDto): Promise<User> {
+    const user = await this.userModel
+      .findOneAndUpdate({ email }, updateUserDto, { new: true })
+      .exec();
+    if (!user) {
+      throw new NotFoundException(`User with email ${email} not found`);
+    }
+    return user;
+  }
+
+  // DELETE
+  async delete(email: string): Promise<void> {
+    const result = await this.userModel.deleteOne({ email }).exec();
+    if (result.deletedCount === 0) {
+      throw new NotFoundException(`User with email ${email} not found`);
+    }
   }
 }
