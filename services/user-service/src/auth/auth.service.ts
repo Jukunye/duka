59e579e0f3b0b8f3149a2aa92f3bc7c0661b92d8
Jukunye <jukunyes@gmail.com>
2024-06-06 21:49:03 +0300

    feat: :sparkles: Add user registration, password hashing, and user management functionality
    Add new functionality to the AuthService for user registration, password hashing, and user management (all, find one, update, delete). Updates the signIn method to use a DTO for validation and throw appropriate exceptions for user not found and incorrect password scenarios.

--- services/user-service/src/auth/auth.service.ts
+++ services/user-service/src/auth/auth.service.ts
@@ -1,6 +1,15 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import {
+  Injectable,
+  NotFoundException,
+  UnauthorizedException
+} from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { UsersService } from 'src/users/users.service';
+import { signInDto } from './dto/signInDto.dto';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { User, UserDocument } from 'src/users/users.schema';
+import { UpdateUserDto } from 'src/users/dto/update-user.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -9,18 +18,43 @@ export class AuthService {
     private jwtService: JwtService
   ) {}
 
-  async signIn(
-    username: string,
-    pass: string
-  ): Promise<{ access_token: string }> {
-    const user = await this.usersService.findOne(username);
-    if (user?.password !== pass) {
-      throw new UnauthorizedException();
-    }
+  async signUp(createUsrDto: CreateUserDto): Promise<User> {
+    const hash = await bcrypt.hash(createUsrDto.password, 10);
+    createUsrDto.password = hash;
+    return await this.usersService.create(createUsrDto);
+  }
 
-    const payload = { sub: user.userId, username: user.username };
+  async signIn(signInDto: signInDto): Promise<{ access_token: string }> {
+    const user = await this.usersService.findOne(signInDto.email);
+
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+    const isPasswordValid = await bcrypt.compare(
+      signInDto.password,
+      user.password
+    );
+    if (!isPasswordValid) {
+      throw new UnauthorizedException('Incorrect password');
+    }
+    const payload = { sub: user._id, email: user.email };
     return {
       access_token: await this.jwtService.signAsync(payload)
     };
+  }
+  async all(): Promise<User[]> {
+    return await this.usersService.findAll();
+  }
+
+  async findOne(email: string): Promise<User> {
+    return await this.usersService.findOne(email);
+  }
+
+  async update(email: string, updateUserDto: UpdateUserDto): Promise<User> {
+    return await this.usersService.update(email, updateUserDto);
+  }
+
+  async delete(email: string): Promise<void> {
+    return await this.usersService.delete(email);
   }
 }
