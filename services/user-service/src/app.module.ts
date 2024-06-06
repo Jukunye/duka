62f0b792e42c979273571d236dad0e29e6aa3c52
Jukunye <jukunyes@gmail.com>
2024-06-06 21:30:42 +0300

    feat: :zap: Add MongooseModule and configure MongoDB connection

--- services/user-service/src/app.module.ts
+++ services/user-service/src/app.module.ts
@@ -3,10 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UsersModule } from './users/users.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [AuthModule, UsersModule],
+  imports: [
+    AuthModule,
+    UsersModule,
+    MongooseModule.forRoot('mongodb://localhost/nest')
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService]
 })
 export class AppModule {}
