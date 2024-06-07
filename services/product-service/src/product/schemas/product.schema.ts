d4506f63f503366e3f77158cf688b63f16355ce3
Jukunye <jukunyes@gmail.com>
2024-06-07 14:02:06 +0300

    Create a product schema

---
+++ services/product-service/src/product/schemas/product.schema.ts
@@ -0,0 +1,16 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+@Schema()
+export class Product extends Document {
+  @Prop({ required: true })
+  name: string;
+
+  @Prop()
+  description: string;
+
+  @Prop({ required: true })
+  price: number;
+}
+
+export const ProductSchema = SchemaFactory.createForClass(Product);
