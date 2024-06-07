5dad8165e532d972802ee02012f008f83fcc79cc
Jukunye <jukunyes@gmail.com>
2024-06-07 13:26:28 +0300

    Create a product service, controller and module

--- services/product-service/src/app.module.ts
+++ services/product-service/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017')],
+  imports: [MongooseModule.forRoot('mongodb://localhost:27017'), ProductModule],
   controllers: [AppController],
   providers: [AppService]
 })
