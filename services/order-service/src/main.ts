7052ddd2de3df89cd7255deb994aa4304d5fa564
Jukunye <jukunyes@gmail.com>
2024-06-08 21:35:56 +0300

    feat: :sparkles: Set up RabbitMQ as the communication mechanism between microservices

--- services/order-service/src/main.ts
+++ services/order-service/src/main.ts
@@ -1,8 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqp://localhost:5672'],
+      queue: 'orders_queue',
+      queueOptions: {
+        durable: false
+      }
+    }
+  });
+  await app.startAllMicroservices();
   await app.listen(3002);
 }
 bootstrap();
--- services/product-service/src/main.ts
+++ services/product-service/src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.connectMicroservice<MicroserviceOptions>({
+    transport: Transport.RMQ,
+    options: {
+      urls: ['amqp://localhost:5672'],
+      queue: 'products_queue',
+      queueOptions: {
+        durable: false
+      }
+    }
+  });
+
+  await app.startAllMicroservices();
   await app.listen(3001);
 }
 bootstrap();
