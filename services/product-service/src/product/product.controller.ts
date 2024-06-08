8cd9acf57d157506368ea61b934d0d42fb73b6f3
Jukunye <jukunyes@gmail.com>
2024-06-08 12:20:44 +0300

    feat: :sparkles: Implement product listing features, including CRUD operations and pagination
    Add product creation functionality, allowing users to add new products. Add product retrieval functionality, including filtering, sorting, and pagination options. Add product update functionality, allowing users to edit product details. Add product deletion functionality, allowing users to remove products from the platform.

--- services/product-service/src/product/product.controller.ts
+++ services/product-service/src/product/product.controller.ts
@@ -1,4 +1,42 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query
+} from '@nestjs/common';
+import { ProductService } from './product.service';
+import { CreateProductDto } from './dto/create-product.dto';
+import { Product } from './schemas/product.schema';
+import { UpdateProuductDto } from './dto/update-product.dto';
 
 @Controller('product')
-export class ProductController {}
+export class ProductController {
+  constructor(private readonly productService: ProductService) {}
+
+  @Post()
+  async create(@Body() createProductDto: CreateProductDto): Promise<Product> {
+    return this.productService.createProduct(createProductDto);
+  }
+
+  @Get()
+  async findAll(@Query() query): Promise<Product[]> {
+    return this.productService.getProducts(query);
+  }
+
+  @Put(':id')
+  async update(
+    @Param('id') id: string,
+    @Body() updateProductDto: UpdateProuductDto
+  ) {
+    return this.productService.updateProduct(id, updateProductDto);
+  }
+
+  @Delete(':id')
+  async remove(@Param('id') id: string): Promise<void> {
+    return this.productService.deleteProduct(id);
+  }
+}
--- services/product-service/src/product/product.service.ts
+++ services/product-service/src/product/product.service.ts
@@ -1,4 +1,56 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { Product } from './schemas/product.schema';
+import { Model } from 'mongoose';
+import { InjectModel } from '@nestjs/mongoose';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProuductDto } from './dto/update-product.dto';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+  constructor(
+    @InjectModel(Product.name) private productModel: Model<Product>
+  ) {}
+
+  async createProduct(createProductDto: CreateProductDto): Promise<Product> {
+    const createdProduct = new this.productModel(createProductDto);
+    return createdProduct.save();
+  }
+
+  async getProducts(query: any): Promise<Product[]> {
+    const {
+      page = 1,
+      limit = 10,
+      sort = 'createdAt',
+      order = 'asc',
+      ...filters
+    } = query;
+    const sortOrder = order === 'asc' ? 1 : -1;
+    const skip = (page - 1) * limit;
+    return this.productModel
+      .find(filters)
+      .sort({ [sort]: sortOrder })
+      .skip(skip)
+      .limit(Number(limit))
+      .exec();
+  }
+
+  async updateProduct(
+    id: string,
+    updateProductDto: UpdateProuductDto
+  ): Promise<Product> {
+    const updatedProduct = await this.productModel
+      .findByIdAndUpdate(id, updateProductDto, { new: true })
+      .exec();
+    if (!updatedProduct) {
+      throw new NotFoundException(`Product with id ${id} not found`);
+    }
+    return updatedProduct;
+  }
+
+  async deleteProduct(id: string): Promise<void> {
+    const result = await this.productModel.findByIdAndDelete(id).exec();
+    if (!result) {
+      throw new NotFoundException(`Product with id ${id} not found`);
+    }
+  }
+}
