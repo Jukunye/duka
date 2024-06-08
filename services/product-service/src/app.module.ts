25ae050b1c8cb7ee48c903ccfdb9995d571be27c
Jukunye <jukunyes@gmail.com>
2024-06-08 12:23:08 +0300

    refactor: change product service port and MongoDB database name

--- services/product-service/src/app.module.ts
+++ services/product-service/src/app.module.ts
@@ -5,7 +5,7 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { ProductModule } from './product/product.module';
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://localhost:27017'), ProductModule],
+  imports: [MongooseModule.forRoot('mongodb://localhost/nest'), ProductModule],
   controllers: [AppController],
   providers: [AppService]
 })
