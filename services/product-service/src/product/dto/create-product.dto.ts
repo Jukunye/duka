01623b587c7043c702f8702d323d77a39b306334
Jukunye <jukunyes@gmail.com>
2024-06-08 12:07:31 +0300

    Create 'CreateProductDto' and 'UpdateProductDto' with optional name, description, and price fields

---
+++ services/product-service/src/product/dto/create-product.dto.ts
@@ -0,0 +1,5 @@
+export class CreateProductDto {
+  readonly name: string;
+  readonly description?: string;
+  readonly price: number;
+}
---
+++ services/product-service/src/product/dto/update-product.dto.ts
@@ -0,0 +1,5 @@
+export class UpdateProuductDto {
+  name?: string;
+  description?: string;
+  price?: number;
+}
